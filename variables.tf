f04a29e3253bd5eff14680bc69fd9db21d4b0911
Jainsi1 <75137939+Jainsi1@users.noreply.github.com>
2023-02-15 11:27:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,4 @@
-# variable "cluster_name" {} 
-# variable "container_name" {} 
-# variable "ecs_service_name" {} 
-# variable "container_definition_file" {} 
 
-variable "containers" {
-  type = map
-  default = {}
-}
-variable "aws_region" {
-}
-
-variable "cluster_name" {}
-variable "publicly_accessible"{}
 variable "file_handler" {}
 variable "function_name" {}
 
@@ -23,9 +10,6 @@ variable "load_balancer_name" {}
 variable "target_group_name" {}
 variable "timeout" {}
 
-
-
-/* variable "repository_name"{} */
 variable "envs" {
   
 }
