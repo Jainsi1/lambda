a788412354c38375c78cfc7c34656e3bd951e518
Jainsi1 <75137939+Jainsi1@users.noreply.github.com>
2023-02-15 11:24:02 +0530

    Delete module/ecs directory

--- module/ecs/log_group.tf
+++
@@ -1,8 +0,0 @@
- /* resource "aws_cloudwatch_log_group" "example_cw_log_group" {
-   name = "/ecs/${var.ecs_service_name}"
-
-   tags = {
-     Environment = "dev"
-     Application = "${var.ecs_service_name}"
-   }
- } */
